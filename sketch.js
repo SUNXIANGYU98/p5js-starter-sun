f9869049b121a6522a7913bea8fc578ce8d52b75
SUN XIANGYU <s3477400485@gmail.com>
2025-05-06 16:05:24 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-/** @typedef {import("./p5/types/index")} Graphics */
-
 /**
+ * @typedef {import("./p5/types").Graphics} Graphics
+ *
  * @typedef {Object} Cubo
  * @property {number} x
  * @property {number} y
@@ -11,14 +11,19 @@
  */
 
 //
-/** @type{Cubo[]} */
+let copie = 100;
 let cubi = [];
-let copie = 400;
 
+/** @type {Graphics} */
 let g;
+
+//
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
+
+  g = createGraphics(100, 100);
+
   for (let i = 0; i < copie; i++) {
     cubi.push({
       x: random(-1000, 1000),
@@ -41,33 +46,40 @@ function setup() {
 }
 
 function draw() {
-  background(100);
+  background(30);
   orbitControl();
-  ambientLight(100);
+  ambientLight(150);
   directionalLight(255, 255, 255, 0.5, 1, -0.5);
+
+  g.background("white");
+  g.text("ciao", 0, g.height);
+  g.textSize(g.height);
+
+  texture(g);
 
   for (let cubo of cubi) {
     push();
     translate(cubo.x, cubo.y, cubo.z);
 
+    // 按颜色旋转
     let colLower = cubo.col.toLowerCase();
     if (["red", "blue", "yellow"].includes(colLower)) {
       rotateX(frameCount * 0.01);
     } else if (["green", "purple", "orange"].includes(colLower)) {
       rotateY(frameCount * 0.01);
-    } else if (["pink", "black", "white"].includes(colLower)) {
+    } else {
       rotateZ(frameCount * 0.01);
     }
 
-    noFill();
-    let edgeColor = color(cubo.col);
-    edgeColor.setAlpha(200);
-    stroke(edgeColor);
-    strokeWeight(3);
-    drawingContext.shadowBlur = 20;
-    drawingContext.shadowColor = edgeColor;
+    // 设置半透明填充
+    let c = color(cubo.col);
+    c.setAlpha(127); // 50% 透明度
+    fill(c);
+
+    // 发光边框
+    stroke(cubo.col);
+    strokeWeight(2);
     box(cubo.size);
-    drawingContext.shadowBlur = 0;
     pop();
   }
 }
